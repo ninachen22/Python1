a6fe44349ac469fe0b20e9acba7305ed5ce99c22
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-11-29 11:31:13 -0500

    exercises 1-3

---
+++ Python1.py
@@ -0,0 +1,33 @@
+# Exercise 1
+print("Hello Python")
+
+# Exercise 2
+True  # type bool
+42  # type int
+False  # type bool
+1.5  # type float
+"Python is fun"  # type str
+"3.5"  # type str
+
+# Exercise 3
+print(1 + 2)
+print(1.5 +3)
+print(1.0 + 2)
+
+print(8 - 4)
+print(8 - 4.0)
+
+print(8 * 4)
+print(8.0 * 4)
+
+print(8 / 4)
+print(8 / 2.0)
+
+print(8 // 3)  # should be 2
+print(8 // 7.0)  # should be 1.0
+
+print(8 % 4.0)
+print(8 % 3)
+
+print(8 ** 2.0)  # expected 64.0
+print(1 ** 4)  # expected 1
